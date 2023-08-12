6a55558b4092690e1d1ed8912e141e730d34c179
burakku6 <leosteam00@gmail.com>
2023-08-11 22:39:39 -0400

    Se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Threading;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var extent = new ExtentReports();
+        var htmlReporter = new ExtentHtmlReporter(Directory.GetCurrentDirectory() + "\\Reportes\\ExtentReport.html");
+        extent.AttachReporter(htmlReporter);
+
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("--start-maximized");
+        IWebDriver driver = new ChromeDriver(options);
+
+        ExtentTest test = extent.CreateTest("Prueba Automatizada", "Descripción de la prueba automatizada");
+
+        try
+        {
+            driver.Navigate().GoToUrl("https://www.oreilly.com/library-access/?language=eyJ...");
+
+            test.Pass("Accediendo a la página");
+
+            Thread.Sleep(3000);
+
+            IWebElement link = driver.FindElement(By.CssSelector("a.orm-Link-root"));
+            link.Click();
+            Thread.Sleep(2000);
+            TakeScreenshot(driver, "Click_Link");
+            test.Pass("Haciendo clic en el enlace");
+
+            IWebElement emailInput = driver.FindElement(By.CssSelector("input#email"));
+            emailInput.SendKeys("20220910@itla.edu.do");
+
+            test.Pass("Ingresando correo electrónico");
+
+            Thread.Sleep(2000);
+
+            IWebElement letsGoButton = driver.FindElement(By.CssSelector("button.orm-Button-root"));
+            letsGoButton.Click();
+            Thread.Sleep(10000);
+            TakeScreenshot(driver, "Click_LetsGo");
+            test.Pass("Haciendo clic en Let's Go");
+
+            IWebElement gotItButton = driver.FindElement(By.CssSelector("a.orm-Button-root._successBtn_dojg6_90.orm-Button-link"));
+            gotItButton.Click();
+            Thread.Sleep(2000);
+            TakeScreenshot(driver, "Click_GotIt");
+            test.Pass("Haciendo clic en Got It");
+
+            IWebElement searchBar = driver.FindElement(By.CssSelector("input#searchBar"));
+            searchBar.SendKeys("C#");
+            Thread.Sleep(3000);
+            TakeScreenshot(driver, "Search_CSharp");
+            test.Pass("Realizando búsqueda y escribiendo 'C#'");
+
+            IWebElement resultLink = driver.FindElement(By.CssSelector("a.css-1xhcqly"));
+            resultLink.Click();
+            Thread.Sleep(5000);
+            TakeScreenshot(driver, "Click_ResultLink");
+            test.Pass("Haciendo clic en el resultado de la búsqueda");
+
+            IWebElement continueButton = driver.FindElement(By.CssSelector("a[data-testid='startContentLink']"));
+            continueButton.Click();
+            Thread.Sleep(3000);
+            TakeScreenshot(driver, "Click_Continue");
+            test.Pass("Haciendo clic en Continue");
+
+            IWebElement myOReillyButton = driver.FindElement(By.XPath("//button[contains(@aria-label, \"My O'Reilly\")]"));
+            myOReillyButton.Click();
+            Thread.Sleep(3000);
+            TakeScreenshot(driver, "Click_MyOReilly");
+            test.Pass("Haciendo clic en My O'Reilly");
+
+            IWebElement signOutButton = driver.FindElement(By.CssSelector("a[data-testid='Desktop-Sign out']"));
+            signOutButton.Click();
+            Thread.Sleep(2000);
+            TakeScreenshot(driver, "Click_SignOut");
+            test.Pass("Haciendo clic en Sign Out");
+
+            test.Pass("Prueba completada con éxito");
+        }
+        catch (Exception ex)
+        {
+            test.Fail("Error durante la ejecución de la prueba: " + ex.Message);
+
+            var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            var screenshotPath = Path.Combine(Directory.GetCurrentDirectory(), "Screenshots", "error_screenshot.png");
+            Directory.CreateDirectory(Path.GetDirectoryName(screenshotPath));
+            screenshot.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+            test.AddScreenCaptureFromPath(screenshotPath, "Captura de Pantalla de Error");
+        }
+        finally
+        {
+            extent.Flush();
+            driver.Quit();
+        }
+    }
+
+    static void TakeScreenshot(IWebDriver driver, string screenshotName)
+    {
+        var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+        var screenshotPath = Path.Combine(Directory.GetCurrentDirectory(), "Screenshots", $"{screenshotName}.png");
+        Directory.CreateDirectory(Path.GetDirectoryName(screenshotPath));
+        screenshot.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+    }
+}
---
+++ bin/Debug/net6.0/Reportes/ReadMe.txt
@@ -0,0 +1,30 @@
+Instrucciones para Ejecutar la Prueba Automatizada:
+Este programa realiza pruebas automatizadas en la plataforma O'Reilly Library Access utilizando Selenium WebDriver y ExtentReports para generar reportes y capturas de pantalla.
+
+***REQUISITOS PREVIOS***
+
+Asegúrate de tener instalado lo siguiente antes de ejecutar el programa:
+
+Google Chrome
+.NET Core SDK
+
+***PASOS PARA EJECUTAR***
+
+Clona este repositorio en tu máquina local.
+
+Abre una terminal y navega a la carpeta del proyecto.
+
+Compila el programa utilizando el comando dotnet.
+
+Ejecuta el programa utilizando el comando dotnet.
+
+Después de la ejecución, un reporte se generará en la carpeta "Reportes" dentro de la carpeta del proyecto. Abre el archivo "ExtentReport.html" en un navegador web para ver el reporte detallado de la prueba automatizada.
+
+Las capturas de pantalla de cada acción realizada durante la prueba se guardarán en la carpeta "Screenshots" dentro de la carpeta del proyecto. Puedes revisar estas capturas para verificar las interacciones realizadas durante la prueba.
+
+Una vez que hayas revisado el reporte y las capturas de pantalla, puedes cerrar el programa y la ventana del navegador.
+
+***NOTAS***
+
+Asegúrate de tener una conexión a Internet activa para que el programa pueda acceder a la plataforma O'Reilly Library Access.
+Si experimentas algún error durante la ejecución de la prueba, revisa el reporte de error en el archivo "ExtentReport.html" y las capturas de pantalla en la carpeta "Screenshots" para identificar posibles problemas.
